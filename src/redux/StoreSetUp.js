686c31328f6bad7b2eb840e79cec9271a817d77b
JAAR91 <jaarriagaramos@gmail.com>
2021-10-19 13:44:00 -0600

    add the rerdux logger, it was removed to make the test

--- src/redux/StoreSetUp.js
+++ src/redux/StoreSetUp.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import reducer, { fetchApiData } from './covid/Covid';
 
 const store = createStore(
   reducer,
-  applyMiddleware(fetchApiData),
+  applyMiddleware(fetchApiData, logger),
 );
 
 export default store;
